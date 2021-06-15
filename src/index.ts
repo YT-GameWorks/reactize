26f947217c32174825e503c30e13d59479e51420
YT-GameWorks <sinha.soham@outlook.com>
2021-06-15 21:43:29 +0530

    added react template

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,56 @@
-import { prompt } from 'enquirer';
-import { CopyFiles } from './util/CopyOver';
-import { Templates } from './util/Templates';
+import { prompt } from "enquirer";
+import { CopyFiles } from "./util/CopyOver";
+import { Templates } from "./util/Templates";
 
 prompt([
-	{
-		type: 'autocomplete',
-		name: 'type',
-		message: 'Choose your project type',
-		choices: ['react', 'react-native', 'electron-react'],
-	},
-	{
-		type: 'input',
-		name: 'projectName',
-		message: 'Name your awesome project!',
-	},
+    {
+        type: "input",
+        name: "projectName",
+        message: "Name your awesome project!",
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "enter the description of your awesome project!",
+    },
+    {
+        type: "autocomplete",
+        name: "type",
+        message: "Choose your project type",
+        choices: ["react", "react-native", "electron-react"],
+    },
+    {
+        type: "confirm",
+        name: "routing",
+        message: "Do you want to add routing?",
+    },
 ]).then((answers: any) => {
-	if (answers.type === 'react') {
-		CopyFiles(Templates.REACT, answers.projectName);
-	} else if (answers.type === 'react-native') {
-		CopyFiles(Templates.REACT_NATIVE, answers.projectName);
-	} else if (answers.type === 'electron-react') {
-		CopyFiles(Templates.ELECTRON_REACT, answers.projectName);
-	}
+    if (answers.type === "react") {
+        CopyFiles(
+            Templates.REACT,
+            answers.projectName,
+            answers.projectDescription,
+            false,
+            false,
+            false
+        );
+    } else if (answers.type === "react-native") {
+        CopyFiles(
+            Templates.REACT_NATIVE,
+            answers.projectName,
+            answers.projectDescription,
+            false,
+            false,
+            false
+        );
+    } else if (answers.type === "electron-react") {
+        CopyFiles(
+            Templates.ELECTRON_REACT,
+            answers.projectName,
+            answers.projectDescription,
+            false,
+            false,
+            false
+        );
+    }
 });
--- src/util/CopyOver.ts
+++ src/util/CopyOver.ts
@@ -1,28 +1,38 @@
 import { Templates } from './Templates';
-import { writeFileSync } from 'fs';
+import * as fs from 'fs-extra';
 import chalk from 'chalk';
 
-import ReactJson from '../../templates/react/react.json';
+export function CopyFiles(
+    Template: Templates,
+    ProjectName: string,
+    ProjectDescription: string,
+    Router: boolean,
+    Redux: boolean,
+    mobx: boolean,
+): void {
+    const cwd = process.cwd();
 
-export function CopyFiles(Template: Templates, ProjectName: string): void {
-	const cwd = process.cwd();
+    if (Template === Templates.REACT) {
+        console.log('Copying react template to current directory...');
 
-	if (Template === Templates.REACT) {
-		console.log('Copying react template to current directory...');
-
-		writeFileSync(cwd + '/react.json', JSON.stringify(ReactJson));
-
-		console.log(
-			chalk.greenBright(
-				'🎉 Successfully copied files to current directory!' +
-					chalk.white(
-						`\n\nThings to do now:\n\ncd ${ProjectName}\n\nnpm install\n\nnpm run dev\n\n\nHappy Coding!`
-					)
-			)
-		);
-	} else if (Template === Templates.REACT_NATIVE) {
-		console.log('React native template coming soon!');
-	} else if (Template === Templates.ELECTRON_REACT) {
-		console.log('Electron template coming soon!');
-	}
+        fs.mkdirSync(cwd + `/${ProjectName}`);
+        fs.copy('templates\\react', cwd + `/${ProjectName}`, (err) => {
+            if (err) {
+                console.log(chalk.red(`copy failed with the ${err}`));
+            } else if (!err) {
+                console.log(
+                    chalk.greenBright(
+                        '🎉 Successfully copied files to current directory!' +
+                            chalk.white(
+                                `\n\nThings to do now:\n\ncd ${ProjectName}\n\nnpm install\n\nnpm run dev\n\n\nHappy Coding!`,
+                            ),
+                    ),
+                );
+            }
+        });
+    } else if (Template === Templates.REACT_NATIVE) {
+        console.log('React native template coming soon!');
+    } else if (Template === Templates.ELECTRON_REACT) {
+        console.log('Electron template coming soon!');
+    }
 }
